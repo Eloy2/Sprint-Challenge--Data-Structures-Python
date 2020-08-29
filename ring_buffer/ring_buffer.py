a59920e4609bba281385a07d4ec34ef371ca23d3
Eloy <eloydgutierrez@gmail.com>
2020-08-28 20:10:41 -0400

    completed ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.store = []
+        self.cur = 0
+    
+    def append(self, value):
+        if len(self.store) == self.capacity:
+            self.store[self.cur] = value
+            self.cur = self.cur + 1
 
-    def append(self, item):
-        pass
+            if self.cur == self.capacity:
+                self.cur = 0
 
+        else:
+            self.store.append(value)
+    
     def get(self):
-        pass
+        return self.store
